2473dd5f9099386144873b343e7c6c991a8884b4
guachochi <davis.bradleyj@gmail.com>
2020-04-13 15:55:06 -0700

    update lib directory to allow .js file to pass tests

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,13 +1,13 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-const Employee = require("Employee")
+const Employee = require("./Employee.js")
 class Engineer extends Employee {
-    constructor(name, id, email, gitHub) {
+    constructor(name, id, email, github) {
         super(name, id, email);
-        this.gitHub = gitHub;
+        this.github = github;
         this.role = "Engineer"
     }
-    getGitHub() {
-        return this.gitHub;
+    getGithub() {
+        return this.github;
     }
 }
 module.exports = Engineer;
